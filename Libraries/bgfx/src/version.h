64722a688010695d67e89f5acba3bdcee4894472
thePaulrus <pauldaly0804@gmail.com>
2026-01-09 13:24:13 -0500

    hopefully fixed libraries

---
+++ Libraries/bgfx/src/version.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2011-2023 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
+ */
+
+/*
+ *
+ * AUTO GENERATED! DO NOT EDIT!
+ *
+ */
+
+#define BGFX_REV_NUMBER 8617
+#define BGFX_REV_SHA1   "9612f6d14732ff7077f0d4aafe53a55dff7f05bd"
